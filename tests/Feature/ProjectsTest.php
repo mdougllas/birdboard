2dc2a9aeedd59cbee4faec05d3061c2c062d4373
Mike Dougllas <mdougllas@gmail.com>
2019-08-04 19:40:52 -0400

    Got back working on this project.

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -13,12 +13,16 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_use_can_create_a_project()
     {
+        $this->withoutExceptionHandling();
         $attributes = [
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph
         ];
 
-        $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
         $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
     }
 }
