6c810bee7ab0fccdb6197036989b246071c1e771
Mike Dougllas <mdougllas@gmail.com>
2019-09-07 22:16:05 -0400

    Created tests and form to delete a project.

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,7 +1,15 @@
-    <div class="card" style="height: 200px">
+    <div class="card relative" style="height: 200px">
         <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-300 pl-4">
             <a href="{{ $project->path() }}">{{ $project->title }}</a>
         </h3>
 
-        <div class="text-gray-600">{{ str_limit($project->description, 145) }}</div>
+        <div class="text-gray-600 mb-4">{{ str_limit($project->description, 145) }}</div>
+
+        <footer class="absolute right-0 bottom-0 mb-2 mr-2">
+            <form action="{{ $project->path() }}" method="POST">
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="text-xs">Delete</button>
+            </form>
+        </footer>
     </div>
