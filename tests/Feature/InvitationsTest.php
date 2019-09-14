ca25bda9ac1f8996bc17ebf7822b8b0a6c463467
Mike Dougllas <mdougllas@gmail.com>
2019-09-14 16:58:02 -0400

    Inserted Birdboard as known word in the folder spellcheck

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -19,7 +19,7 @@ class InvitationsTest extends TestCase
 
         $project->invite($newUser = factory(User::class)->create());
 
-        $this-signIn($newUser);
+        $this->signIn($newUser);
         $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
 
         $this->assertDatabaseHas('tasks', $task);
